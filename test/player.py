45bcb3a1a3d3fddd9491255a7a3c3b20e6ebf48b
Danielauler <daniel-auler@hotmail.com>
2020-05-10 16:51:57 -0300

    vai

--- test/player.py
+++ test/player.py
@@ -5,9 +5,5 @@ pygame.mixer.init()
 pygame.mixer.music.load(file)
 pygame.mixer.music.play()
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False # Here we exit the Loop and execute what after.
-pygame.quit()
+while pygame.mixer.music.get_busy(): 
+  pygame.time.Clock().tick(10)
